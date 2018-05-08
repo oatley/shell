b574a0cbd0091f0734b83ed1fbf99cc7c89dd0ce
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-08 11:40:23 -0400

    trying again

--- game.js
+++ game.js
@@ -13,6 +13,8 @@ var config = {
 }
 var game = new Phaser.Game(config);
 
+game.state.add('controller', controller);
+
 // Player objects
 var player1;
 var player2;
@@ -84,49 +86,6 @@ function toggleFullscreen() {
     }
 }
 
-// Set player 1 controls
-function player1Controls() {
-    // Keyboard controls
-    upPlayer1Button = game.input.keyboard.addKey(Phaser.Keyboard.W);
-    downPlayer1Button = game.input.keyboard.addKey(Phaser.Keyboard.S);
-    leftPlayer1Button = game.input.keyboard.addKey(Phaser.Keyboard.A);
-    rightPlayer1Button = game.input.keyboard.addKey(Phaser.Keyboard.D);
-    attackPlayer1Button = game.input.keyboard.addKey(Phaser.Keyboard.F);
-    switchPlayer1Button = game.input.keyboard.addKey(Phaser.Keyboard.E);
-    musicMuteButton = game.input.keyboard.addKey(Phaser.Keyboard.P);
-    musicPlus1Button = game.input.keyboard.addKey(Phaser.Keyboard.EQUALS);
-    musicPlus2Button = game.input.keyboard.addKey(Phaser.Keyboard.PLUS);
-    musicMinus1Button = game.input.keyboard.addKey(Phaser.Keyboard.MINUS);
-    musicMinus2Button = game.input.keyboard.addKey(Phaser.Keyboard.UNDERSCORE);
-
-    resetGameButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-    musicMuteButton.onDown.add(muteMusicVolume, this);
-    musicPlus1Button.onDown.add(increaseMusicVolume, this);
-    musicPlus2Button.onDown.add(increaseMusicVolume, this);
-    musicMinus1Button.onDown.add(decreaseMusicVolume, this);
-    musicMinus2Button.onDown.add(decreaseMusicVolume, this);
-
-    //switchPlayer1Button.onDown.add(switchAllLevel, this);
-game.add.group();
-    // Xbox controller controls
-
-}
-
-// Set player 2 controls
-function player2Controls() {
-    // Keyboard controls
-    upPlayer2Button = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-    downPlayer2Button = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-    leftPlayer2Button = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-    rightPlayer2Button = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-    attackPlayer2Button = game.input.keyboard.addKey(Phaser.Keyboard.L);
-    //switchPlayer2Button = game.input.keyboard.addKey(Phaser.Keyboard.K);
-
-    // Xbox controller controls
-
-}
-
 // Load images
 function loadImages() {
     // Load images background
@@ -150,6 +109,7 @@ function loadImages() {
     game.load.image('sci', 'assets/sprites/ui/ui_portrait_sci03.png');
     game.load.image('jan', 'assets/sprites/ui/ui_portrait_jan03.png');
 }
+
 
 // Load spritesheets
 function loadSpriteSheets() {
@@ -822,8 +782,9 @@ function create() {
     music.play();
 
     // Setup controls
-    player1Controls();
-    player2Controls();
+    game.state.start('controller');
+    controller.player1Controls();
+    controller.player2Controls();
     cursors = game.input.keyboard.createCursorKeys();
     game.input.mouse.capture = true;
 
