a997fee6adee3d85b9e528b06145f1625566ec39
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-08 11:52:00 -0400

    start load

--- game.js
+++ game.js
@@ -16,6 +16,7 @@ var game = new Phaser.Game(config);
 // Load code into game in order
 game.state.add('controller', controller);
 game.state.add('load', load);
+game.state.start('load');
 
 // Run the code so you have access to the WTF DOES THIS EVEN DO? THAT THE ABOVE DOESN"T ALREADY DO?
 //game.state.start('controller');
