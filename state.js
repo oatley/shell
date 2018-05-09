5c00ef2357f296bc9e11f499dbb509001cc4c0c2
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-09 00:07:04 -0400

    Cleanplatforms in winner screen

--- state.js
+++ state.js
@@ -6,6 +6,7 @@ let isGameOverScreen = false;
 let state = {
     gameOver: function () {
         clean.cleanPortraits();
+        clean.cleanPlatforms();
         let screenGameOver = game.add.sprite(game.world.width/2, game.world.height/2, 'youwin');
         screenGameOver.anchor.setTo(0.5, 0.5);
         displayScreensGroup.add(screenGameOver);
