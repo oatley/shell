b4248cdc0da7e9c27727c488946ce7577df0ae10
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2018-05-08 21:48:53 -0400

    winner portrait location

--- state.js
+++ state.js
@@ -13,7 +13,7 @@ var state = {
             console.log('you win player1');
             player1Group.forEach(function(player) {
                 console.log(player.model);
-                player.portrait = game.add.sprite(game.world.width / 2, game.world.height / 2 - 128, player.model + player.playerStage);
+                player.portrait = game.add.sprite(game.world.width / 2, game.world.height / 2 - 1, player.model + player.playerStage);
                 player.portrait.anchor.setTo(0.5, 0.5);
                 portraitsGroup.add(player.portrait);
                 game.world.bringToTop(portraitsGroup);
